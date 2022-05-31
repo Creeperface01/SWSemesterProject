ef54848284f859f18c3afea4101349c4125df7f4
Snizhana <snizhana.liashevska@lemenio.com>
2022-05-31 19:49:26 +0900

    Change

--- app/views.py
+++ app/views.py
@@ -6,3 +6,13 @@ from app import app
 @app.route('/')
 def home() -> str:
     return render_template('index.html')
+
+
+@app.route('/signup')
+def signup() -> str:
+    return render_template('signup.html')
+
+
+@app.route('/profile')
+def profile() -> str:
+    return render_template('profile.html')
